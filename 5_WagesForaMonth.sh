ed942a0f86d772e5232f62a12819c25258df32bc
koshtirushikesh <Enter git registered email id>
2022-11-25 22:25:47 +0530

    Wages for a Month

---
+++ 5_WagesForaMonth.sh
@@ -0,0 +1,23 @@
+isPresent=$((RANDOM%3));
+perHourSalary=20;
+workingHour=0;
+NumberofWorkingDay=20;
+
+for (( day = 0; day <=NumberofWorkingDay; day++ )); do
+
+  if [ $isPresent -eq 0 ]
+  then
+  	echo "Employee is absent";
+  	workingHour=0;
+  elif [ $isPresent -eq 1 ]
+  then
+  	echo "Employee is present";
+  	workingHour=8;
+  else
+  	echo "Employee is working as part time";
+  	workingHour=4;
+  fi
+  salary=$(($perHourSalary * $workingHour));
+  echo "Employee has earned $salary $ today";
+
+done
